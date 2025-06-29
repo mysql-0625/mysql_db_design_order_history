6804ae47127ee40cc611aa1b533925705eb7c908
mhasan8801 <hasanmuhammad197@gmail.com>
2025-06-29 11:29:19 +0700

    add data orders & order details

--- mysql_order_history.sql
+++ mysql_order_history.sql
@@ -45,7 +45,32 @@ CREATE TABLE order_details (
 
 DESC order_details;
 
+-- Insert data orders
+INSERT INTO orders 
+(id, created_at, seller_id, seller_name, buyer_id, buyer_name, shipping_name, shipping_address, shipping_phone,
+logistic_id, logistic_name, payment_method_id, payment_method_name, total_quantity, total_weight, total_product_amount, 
+total_shipping_cost, total_shopping_amount, service_charge, total_amount)
+VALUES 
+('234', NOW(), 'adidas_indonesia', 'Adidas Indonesia', 'mhasan', 'M. Hasan', 'Ahmad Saifur', 'Jalan Raya Cinta, Jakarta, DKI Jakarta, 14732', '098123123123',
+'siwosh', 'Si Woshhh', 'debit_online', 'Debit Online', 5, 5360, 508500, 
+60000, 568500, 1000, 569500);
 
+SELECT * FROM orders;
+
+-- Insert data order details
+INSERT INTO order_details (id, order_id, product_id, product_name, product_weight, product_price, quantity, total_amount)
+					VALUES ('1', '234', 'P001', 'Adidas Samba 320', 2300, 177900, 2, 355800);
+
+INSERT INTO order_details (id, order_id, product_id, product_name, product_weight, product_price, quantity, total_amount)
+					VALUES ('2', '234', 'P002', 'Running Adidas MS10', 500, 98900, 1, 98900);
+
+INSERT INTO order_details(id, order_id, product_id, product_name, product_weight, product_price, quantity, total_amount)
+					VALUES ('3', '234', 'P003', 'Sport BD031 Adidas', 54, 9900, 1, 9900);
+
+INSERT INTO order_details(id, order_id, product_id, product_name, product_weight, product_price, quantity, total_amount)
+					VALUES ('4', '234', 'P004', 'Adidas Sneakers 110', 198, 43900, 1, 43900);
+
+SELECT * FROM order_details;
 
 
 
